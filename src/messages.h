5ec7bdaff9f1080a1fae4fdd2ec4fb24d938bfab
Tomasz Bekas <tomasz.bekas@gmail.com>
2025-12-18 12:39:38 +0100

    Fix auto scan not starting after dc

--- src/messages.h
+++ src/messages.h
@@ -1,21 +1,21 @@
 #pragma once
 
 enum class BLEAutoScanNotification : uint8_t {
-  Auto = 0,
-  Enabled = 1,
-  Disabled = 2,
-  ScanStopped = 3,
-  ScanFinished = 4
+  Auto = 1,
+  Enabled = 2,
+  Disabled = 3,
+  ScanStopped = 4,
+  ScanFinished = 5,
 };
 
 enum class BLEUserCallbackKind : uint8_t {
-  ControllerConnecting = 0,
-  ControllerConnectionFailed = 1,
-  ControllerConnected = 2,
-  ControllerDisconnected = 3,
+  ControllerConnecting = 1,
+  ControllerConnectionFailed = 2,
+  ControllerConnected = 3,
+  ControllerDisconnected = 4,
 
-  ScanStarted = 4,
-  ScanStopped = 5,
+  ScanStarted = 5,
+  ScanStopped = 6,
 };
 
 struct BLEUserCallback {
